cc25736fad5a5878e4f58543eebd6342a9e2dede
AbdelwahebLaabidi <abidi.abdelwaheb@hotmail.com>
2023-12-21 16:20:31 +0100

    Movie App with Redux

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AddMovie from './Components/AddMovie';
+import FilterMovie from './Components/FilterMovie';
+import ListMovie from './Components/ListMovie';
+import NavMovie from './Components/NavMovie';
+import Home from './Components/Home';
+
+function App() {
+  
+  return (
+    <div>
+      <NavMovie/>
+
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/ListMovie' element={<ListMovie/>}/>
+      </Routes>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Redux/Actions.js
@@ -0,0 +1,46 @@
+import { ADDMOVIE, DELETEMOVIE, HANDLECHANGEMENT, HANDLERATE, RESETFILTER } from "./ActionTypes"
+
+export const addMovie =(payload)=>{
+    return(
+        
+        {
+            type : ADDMOVIE,
+            payload
+        }
+    )
+}
+
+export const deleteMovie=(payload)=>{
+    return(
+        {
+            type : DELETEMOVIE,
+            payload
+        }
+    )
+}
+
+export const handleChangement=(payload)=>{
+    return(
+        {
+            type : HANDLECHANGEMENT,
+            payload
+        }
+    )
+}
+
+export const handleRate=(payload)=>{
+    return(
+        {
+            type :HANDLERATE,
+            payload
+        }
+    )
+}
+
+export const resetFilter=()=>{
+    return(
+        {
+            type: RESETFILTER
+        }
+    )
+}
